cfa7a06e9b18349065e64616f5023971704a1e5f
ExMix <rahuba.youri@mapswithme.com>
2015-01-23 19:03:52 +0300

    [drape] render bookmarks, search and api points

--- iphone/Maps/Bookmarks/BookmarksRootVC.mm
+++ iphone/Maps/Bookmarks/BookmarksRootVC.mm
@@ -89,7 +89,10 @@
     // Invert visibility
     bool visible = !cat->IsVisible();
     cell.imageView.image = [UIImage imageNamed:(visible ? @"ic_show_light" : @"ic_hide_light")];
-    cat->SetVisible(visible);
+    {
+      BookmarkCategory::Guard guard(*cat);
+      guard.m_controller.SetIsVisible(visible);
+    }
     cat->SaveToKMLFile();
   }
 }
@@ -116,7 +119,7 @@
     NSString * title = @(cat->GetName().c_str());
     cell.textLabel.text = [self truncateString:title toWidth:(self.tableView.width - 122) withFont:cell.textLabel.font];
     cell.imageView.image = [UIImage imageNamed:(cat->IsVisible() ? @"ic_show_light" : @"ic_hide_light")];
-    cell.detailTextLabel.text = [NSString stringWithFormat:@"%ld", cat->GetBookmarksCount() + cat->GetTracksCount()];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%ld", cat->GetUserMarkCount() + cat->GetTracksCount()];
   }
   return cell;
 }
