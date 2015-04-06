8a3c396fbb3ae10eb8c823f80a866180065f79c7
Lev Dragunov <l.dragunov@corp.mail.ru>
2015-04-06 16:19:56 +0300

    Mapsme old routing algos

--- map/framework.cpp
+++ map/framework.cpp
@@ -15,7 +15,8 @@
 #include "../defines.hpp"
 
 #include "../routing/route.hpp"
-#include "../routing/osrm_router.hpp"
+#include "../routing/astar_router.hpp"
+//#include "../routing/osrm_router.hpp"
 
 #include "../search/search_engine.hpp"
 #include "../search/result.hpp"
@@ -270,10 +271,11 @@ Framework::Framework()
   m_storage.Init(bind(&Framework::UpdateAfterDownload, this, _1, _2));
   LOG(LDEBUG, ("Storage initialized"));
 
-  m_routingSession.SetRouter(new OsrmRouter(&m_model.GetIndex(), [this]  (m2::PointD const & pt)
+  m_routingSession.SetRouter(new AStarRouter(&m_model.GetIndex()));
+  /*m_routingSession.SetRouter(new OsrmRouter(&m_model.GetIndex(), [this]  (m2::PointD const & pt)
   {
     return GetSearchEngine()->GetCountryFile(pt);
-  }));
+  }));*/
   LOG(LDEBUG, ("Routing engine initialized"));
 
   LOG(LINFO, ("System languages:", languages::GetPreferred()));
