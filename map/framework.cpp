259c70ad72d67146316b9909fb4a57863cf68231
r.kuznetsov <r.kuznetsov@corp.mail.ru>
2015-04-23 15:28:55 +0300

    Review fixes

--- map/framework.cpp
+++ map/framework.cpp
@@ -171,7 +171,7 @@ Framework::Framework()
 {
   m_activeMaps.reset(new ActiveMapsLayout(*this));
   m_globalCntTree = storage::CountryTree(m_activeMaps);
-  m_storageAccessor = move(make_unique_dp<StorageBridge>(m_activeMaps));
+  m_storageAccessor = make_unique_dp<StorageBridge>(m_activeMaps);
   // Init strings bundle.
   // @TODO. There are hardcoded strings below which are defined in strings.txt as well.
   // It's better to use strings form strings.txt intead of hardcoding them here.
@@ -1341,18 +1341,18 @@ void Framework::CreateDrapeEngine(ref_ptr<dp::OGLContextFactory> contextFactory,
 
   df::DrapeEngine::Params p(contextFactory,
                             make_ref(&m_stringsBundle),
-                            make_ref<gui::StorageAccessor>(m_storageAccessor),
+                            make_ref(m_storageAccessor),
                             df::Viewport(0, 0, w, h),
                             df::MapDataProvider(idReadFn, featureReadFn, resolveCountry),
                             vs);
 
-  m_drapeEngine = move(make_unique_dp<df::DrapeEngine>(p));
+  m_drapeEngine = make_unique_dp<df::DrapeEngine>(p);
   OnSize(w, h);
 }
 
 ref_ptr<df::DrapeEngine> Framework::GetDrapeEngine()
 {
-  return make_ref<df::DrapeEngine>(m_drapeEngine);
+  return make_ref(m_drapeEngine);
 }
 
 void Framework::DestroyDrapeEngine()
