5df8fdbcd8cb33b67b6c25bcdc2eb8fce5282516
ExMix <rahuba.youri@mapswithme.com>
2015-05-12 14:25:01 +0300

    review fixes

--- android/src/com/mapswithme/maps/MwmApplication.java
+++ android/src/com/mapswithme/maps/MwmApplication.java
@@ -162,14 +162,14 @@ public class MwmApplication extends android.app.Application implements ActiveCou
     System.loadLibrary("mapswithme");
   }
 
-  public void runNativeFunctorOnUIThread(final long functionPointer)
+  public void runNativeFunctorOnUiThread(final long functorPointer)
   {
     mMainLoopHandler.post(new Runnable()
     {
       @Override
       public void run()
       {
-        nativeCallOnUIThread(functionPointer);
+        runNativeFunctor(functorPointer);
       }
     });
   }
@@ -179,7 +179,7 @@ public class MwmApplication extends android.app.Application implements ActiveCou
                                  String flavorName, String buildType,
                                  boolean isYota, boolean isTablet);
 
-  private native void nativeCallOnUIThread(long functorPointer);
+  private native void runNativeFunctor(final long functorPointer);
   private native void nativeAddLocalization(String name, String value);
 
   /**
