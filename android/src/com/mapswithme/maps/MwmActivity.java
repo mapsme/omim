1c4296e225d703a1d6bb2f41822bfabc1e6db2d1
ExMix <rahuba.youri@mapswithme.com>
2015-05-28 14:01:46 +0300

    rebase fix

--- android/src/com/mapswithme/maps/MwmActivity.java
+++ android/src/com/mapswithme/maps/MwmActivity.java
@@ -1105,11 +1105,11 @@ public class MwmActivity extends BaseMwmFragmentActivity
       break;
     case R.id.map_button_plus:
       AlohaHelper.logClick(AlohaHelper.ZOOM_IN);
-      mMapFragment.nativeScale(3.0 / 2);
+      mMapFragment.nativeScalePlus();
       break;
     case R.id.map_button_minus:
       AlohaHelper.logClick(AlohaHelper.ZOOM_OUT);
-      mMapFragment.nativeScale(2 / 3.0);
+      mMapFragment.nativeScaleMinus();
       break;
     case R.id.yop_it:
       final double[] latLon = Framework.getScreenRectCenter();
--- iphone/Maps/Classes/CustomViews/MapViewControls/MWMMapViewControlsManager.h
+++ iphone/Maps/Classes/CustomViews/MapViewControls/MWMMapViewControlsManager.h
@@ -41,5 +41,6 @@
 - (void)routingNavigation;
 - (void)handleRoutingError;
 - (void)setRouteBuildingProgress:(CGFloat)progress;
+- (void)setMyPositionMode:(location::EMyPositionMode)mode;
 
 @end
