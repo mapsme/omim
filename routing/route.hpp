92b08560c6337515e1bdb38a99ebd84b710a5740
r.kuznetsov <r.kuznetsov@corp.mail.ru>
2015-06-09 16:59:36 +0300

    Review fixes

--- routing/route.hpp
+++ routing/route.hpp
@@ -118,6 +118,8 @@ private:
   double GetPolySegAngle(size_t ind) const;
   TTurns::const_iterator GetCurrentTurn() const;
 
+  double GetMercatorDistanceFromBegin() const;
+
 private:
   friend string DebugPrint(Route const & r);
 
