8e59cd153c4e45ca32c08891de39973386d062e4
ExMix <rahuba.youri@mapswithme.com>
2015-06-15 18:55:17 +0300

    [drape] selection object small refactoring. Empty state added

--- map/framework.cpp
+++ map/framework.cpp
@@ -1616,7 +1616,7 @@ void Framework::ActivateUserMark(UserMark const * mark, bool needAnim)
       else if (type == UserMark::Type::POI)
         object = df::SelectionShape::OBJECT_POI;
 
-      CallDrapeFunction(bind(&df::DrapeEngine::SelectObject, _1, object, pt));
+      CallDrapeFunction(bind(&df::DrapeEngine::SelectObject, _1, object, pt, needAnim));
     }
     else
     {
