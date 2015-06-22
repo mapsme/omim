676e3ea7f29949bda3aab5a1a5d1af8fff950407
ExMix <rahuba.youri@mapswithme.com>
2015-06-22 21:43:18 +0300

    rebase fixes

--- iphone/Maps/Classes/CustomViews/MapViewControls/ZoomButtons/MWMZoomButtons.mm
+++ iphone/Maps/Classes/CustomViews/MapViewControls/ZoomButtons/MWMZoomButtons.mm
@@ -48,11 +48,6 @@ extern NSString * const kAlohalyticsTapEventKey;
   self.zoomView.bottomBound = bound;
 }
 
-- (void)zoom:(CGFloat)scale
-{
-  GetFramework().Scale(scale);
-}
-
 - (void)zoomIn
 {
   [Alohalytics logEvent:kAlohalyticsTapEventKey withValue:@"+"];
@@ -93,7 +88,7 @@ extern NSString * const kAlohalyticsTapEventKey;
   UIView * const superview = self.zoomView.superview;
   CGFloat const translation = -[sender translationInView:superview].y / superview.bounds.size.height;
 
-  CGFloat const scaleFactor = pow(2, translation);
+  CGFloat const scaleFactor = exp(translation);
   GetFramework().Scale(scaleFactor, false);
 }
 
--- iphone/Maps/Classes/MapsAppDelegate.mm
+++ iphone/Maps/Classes/MapsAppDelegate.mm
@@ -207,7 +207,7 @@ void InitLocalizedStrings()
   
   Framework & f = GetFramework();
   application.applicationIconBadgeNumber = f.GetCountryTree().GetActiveMapLayout().GetOutOfDateCount();
-  f.GetLocationState()->InvalidatePosition();
+  f.InvalidateMyPosition();
 
   return returnValue;
 }
