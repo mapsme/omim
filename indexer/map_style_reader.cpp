174fe19854c70353aa629018f82a08625eb7234c
r.kuznetsov <r.kuznetsov@corp.mail.ru>
2015-07-08 17:41:41 +0300

    Review fixes

--- indexer/map_style_reader.cpp
+++ indexer/map_style_reader.cpp
@@ -53,11 +53,6 @@ MapStyle StyleReader::GetCurrentStyle()
   return static_cast<MapStyle>(mapStyle);
 }
 
-string StyleReader::GetCurrentStyleSuffix()
-{
-  return GetStyleSuffix(GetCurrentStyle());
-}
-
 ReaderPtr<Reader> StyleReader::GetDrawingRulesReader()
 {
   string const rulesFile = string("drules_proto") + GetStyleSuffix(GetCurrentStyle()) + ".bin";
