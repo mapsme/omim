bf813b19d067deb0bd9ee0591ea917cb642fcb7c
ExMix <rahuba.youri@mapswithme.com>
2015-08-15 17:04:21 +0300

    [drape] optimizations

--- map/framework.cpp
+++ map/framework.cpp
@@ -882,6 +882,7 @@ void Framework::MemoryWarning()
 {
   LOG(LINFO, ("MemoryWarning"));
   ClearAllCaches();
+  SharedBufferManager::instance().clearReserved();
 }
 
 void Framework::EnterBackground()
