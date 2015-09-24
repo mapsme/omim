476ce8be63e84eae1e8d8969f43e20980165a70e
Roman Kuznetsov <r.kuznetsov@corp.mail.ru>
2015-09-24 16:58:58 +0300

    Rebase fixes after migration to github

--- android/src/com/mapswithme/maps/widget/placepage/PlacePageView.java
+++ android/src/com/mapswithme/maps/widget/placepage/PlacePageView.java
@@ -789,7 +789,6 @@ public class PlacePageView extends RelativeLayout implements View.OnClickListene
         }
       });
     }
-    Framework.invalidate();
   }
 
   private void selectBookmarkSet()
