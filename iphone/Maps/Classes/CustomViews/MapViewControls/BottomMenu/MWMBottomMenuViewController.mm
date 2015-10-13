e13e23529bd5978a085e64dec60d3e527403fa9f
Ilya Grechuhin <i.grechuhin@gmail.com>
2015-10-13 12:57:07 +0300

    [ios] Added widgets layout routine.

--- iphone/Maps/Classes/CustomViews/MapViewControls/BottomMenu/MWMBottomMenuViewController.mm
+++ iphone/Maps/Classes/CustomViews/MapViewControls/BottomMenu/MWMBottomMenuViewController.mm
@@ -1,4 +1,5 @@
 #import "Common.h"
+#import "EAGLView.h"
 #import "MapsAppDelegate.h"
 #import "MapViewController.h"
 #import "MWMActivityViewController.h"
@@ -500,6 +501,7 @@ typedef NS_ENUM(NSUInteger, MWMBottomMenuViewCell)
 - (void)setLeftBound:(CGFloat)leftBound
 {
   ((MWMBottomMenuView *)self.view).leftBound = leftBound;
+  ((EAGLView *)self.controller.view).widgetsManager.leftBound = leftBound;
 }
 
 - (CGFloat)leftBound
