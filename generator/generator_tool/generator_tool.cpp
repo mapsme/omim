1503431b22ea6ca0d9468eeadc9740fdfb85aab6
Maxim Pimenov <m@maps.me>
2015-10-23 17:47:36 +0300

    Review fixes.

--- generator/generator_tool/generator_tool.cpp
+++ generator/generator_tool/generator_tool.cpp
@@ -187,7 +187,7 @@ int main(int argc, char ** argv)
     {
       LOG(LINFO, ("Generating index for", datFile));
 
-      if (!indexer::BuildIndexFromDatFile(datFile, FLAGS_intermediate_data_path + country))
+      if (!indexer::BuildIndexFromDataFile(datFile, FLAGS_intermediate_data_path + country))
         LOG(LCRITICAL, ("Error generating index."));
     }
 
@@ -195,7 +195,7 @@ int main(int argc, char ** argv)
     {
       LOG(LINFO, ("Generating search index for ", datFile));
 
-      if (!indexer::BuildSearchIndexFromDatFile(datFile, true))
+      if (!indexer::BuildSearchIndexFromDataFile(datFile, true))
         LOG(LCRITICAL, ("Error generating search index."));
 
       LOG(LINFO, ("Generating rank table for ", datFile));
