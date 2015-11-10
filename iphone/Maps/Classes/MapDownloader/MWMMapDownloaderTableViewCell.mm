42d897cd9f64d53e454cc2b72babc8c22e2306d4
Илья Гречухин <i.grechuhin@gmail.com>
2015-11-10 11:29:39 +0300

    [ios] Rebased to master.

--- iphone/Maps/Classes/MapDownloader/MWMMapDownloaderTableViewCell.mm
+++ iphone/Maps/Classes/MapDownloader/MWMMapDownloaderTableViewCell.mm
@@ -1,7 +1,7 @@
 #import "MWMCircularProgress.h"
 #import "MWMMapDownloaderTableViewCell.h"
 
-@interface MWMMapDownloaderTableViewCell () <MWMCircularProgressDelegate>
+@interface MWMMapDownloaderTableViewCell () <MWMCircularProgressProtocol>
 
 @property (nonatomic) MWMCircularProgress * progressView;
 
@@ -14,7 +14,17 @@
 - (void)awakeFromNib
 {
   [super awakeFromNib];
-  self.progressView = [[MWMCircularProgress alloc] initWithParentView:self.stateWrapper delegate:self];
+  self.progressView = [[MWMCircularProgress alloc] initWithParentView:self.stateWrapper];
+  self.progressView.delegate = self;
+  [self.progressView setImage:[UIImage imageNamed:@"ic_download"] forState:MWMCircularProgressStateNormal];
+  [self.progressView setImage:[UIImage imageNamed:@"ic_download_press"] forState:MWMCircularProgressStateHighlighted];
+  [self.progressView setImage:[UIImage imageNamed:@"ic_download"] forState:MWMCircularProgressStateSelected];
+  [self.progressView setImage:[UIImage imageNamed:@"ic_download_press"] forState:MWMCircularProgressStateSelectedHighlighted];
+  [self.progressView setImage:[UIImage imageNamed:@"ic_close_spinner"] forState:MWMCircularProgressStateProgress];
+  [self.progressView setImage:[UIImage imageNamed:@"ic_close_spinner_press"] forState:MWMCircularProgressStateProgressHighlighted];
+  [self.progressView setImage:[UIImage imageNamed:@"ic_download_error"] forState:MWMCircularProgressStateFailed];
+  [self.progressView setImage:[UIImage imageNamed:@"ic_download_error_press"] forState:MWMCircularProgressStateFailedHighlighted];
+  [self.progressView setImage:[UIImage imageNamed:@"ic_check"] forState:MWMCircularProgressStateCompleted];
 }
 
 - (CGFloat)estimatedHeight
