10d152a8b3e22a65eec9ff977ca492b821980324
Daria Volvenkova <d.volvenkova@corp.mail.ru>
2015-11-17 10:58:20 +0300

    Fixed scaling in 3d for follow-and-rotate mode.
    Conflicts:
    	drape_frontend/frontend_renderer.cpp
    	map/framework.cpp

--- drape_frontend/frontend_renderer.cpp
+++ drape_frontend/frontend_renderer.cpp
@@ -486,7 +486,7 @@ void FrontendRenderer::OnResize(ScreenBase const & screen)
 {
   m2::RectD const viewportRect = screen.isPerspective() ? screen.PixelRectIn3d() : screen.PixelRect();
 
-  m_myPositionController->SetPixelRect(screen.PixelRect());
+  m_myPositionController->SetPixelRect(viewportRect);
   m_viewport.SetViewport(0, 0, screen.GetWidth(), screen.GetHeight());
   m_contextFactory->getDrawContext()->resize(viewportRect.SizeX(), viewportRect.SizeY());
   RefreshProjection();
@@ -1170,6 +1170,8 @@ void FrontendRenderer::UpdateScene(ScreenBase const & modelView)
   ResolveZoomLevel(modelView);
   TTilesCollection tiles;
   ResolveTileKeys(modelView, tiles);
+
+  m_myPositionController->UpdatePixelPosition(modelView);
 
   m_overlayTree->ForceUpdate();
   auto removePredicate = [this](drape_ptr<RenderGroup> const & group)
