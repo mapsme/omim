dbf77e41106848b06191f6254ab0a6ece0671a8c
Daria Volvenkova <d.volvenkova@corp.mail.ru>
2015-11-19 15:49:49 +0300

    Recover perspective in case of drape engine reinitialization.
    Conflicts:
    	drape_frontend/message.hpp
    	map/framework.cpp

--- drape_frontend/frontend_renderer.cpp
+++ drape_frontend/frontend_renderer.cpp
@@ -377,7 +377,7 @@ void FrontendRenderer::AcceptMessage(ref_ptr<Message> message)
       {
         m_myPositionController->DeactivateRouting();
         if (m_enable3dInNavigation)
-          AddUserEvent(Disable3dModeEvent());
+          AddUserEvent(DisablePerspectiveEvent());
       }
       break;
     }
@@ -388,7 +388,7 @@ void FrontendRenderer::AcceptMessage(ref_ptr<Message> message)
       m_myPositionController->NextMode(!m_enable3dInNavigation ? msg->GetPreferredZoomLevel()
                                                                : msg->GetPreferredZoomLevelIn3d());
       if (m_enable3dInNavigation)
-        AddUserEvent(Enable3dModeEvent(msg->GetRotationAngle(), msg->GetAngleFOV()));
+        AddUserEvent(EnablePerspectiveEvent(msg->GetRotationAngle(), msg->GetAngleFOV(), true));
       break;
     }
 
@@ -467,6 +467,13 @@ void FrontendRenderer::AcceptMessage(ref_ptr<Message> message)
 
       RefreshBgColor();
 
+      break;
+    }
+
+  case Message::EnablePerspective:
+    {
+      ref_ptr<EnablePerspectiveMessage> const msg = message;
+      AddUserEvent(EnablePerspectiveEvent(msg->GetRotationAngle(), msg->GetAngleFOV(), false));
       break;
     }
 
