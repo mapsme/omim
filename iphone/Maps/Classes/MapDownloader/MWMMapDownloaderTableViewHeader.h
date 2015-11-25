ce1bf6d1e6d681f7be4f43985847b2cfd7d8fb1e
Илья Гречухин <i.grechuhin@gmail.com>
2015-11-25 12:19:27 +0300

    [ios] Removed section collapse capability.

--- iphone/Maps/Classes/MapDownloader/MWMMapDownloaderTableViewHeader.h
+++ iphone/Maps/Classes/MapDownloader/MWMMapDownloaderTableViewHeader.h
@@ -1,20 +1,11 @@
 @class MWMMapDownloaderTableViewHeader;
 
-@protocol MWMMapDownloaderTableViewHeaderProtocol <NSObject>
-
-- (void)expandButtonPressed:(MWMMapDownloaderTableViewHeader *)sender;
-
-@end
-
 @interface MWMMapDownloaderTableViewHeader : UIView
 
 + (CGFloat)height;
 
 @property (weak, nonatomic) IBOutlet UILabel * title;
 
-@property (weak, nonatomic) id<MWMMapDownloaderTableViewHeaderProtocol> delegate;
-@property (nonatomic) BOOL expanded;
-@property (nonatomic) NSInteger section;
 @property (nonatomic) BOOL lastSection;
 
 @end
