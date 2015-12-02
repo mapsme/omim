c55049841fb7ae085d91010faa02a0d8c79f73b3
Daria Volvenkova <d.volvenkova@corp.mail.ru>
2015-12-02 15:09:21 +0300

    Billboard render pass removed.

--- drape_frontend/frontend_renderer.hpp
+++ drape_frontend/frontend_renderer.hpp
@@ -210,7 +210,6 @@ private:
   FeatureID GetVisiblePOI(m2::PointD const & pixelPoint) const;
   FeatureID GetVisiblePOI(m2::RectD const & pixelRect) const;
 
-  bool IsBillboardProgram(int programIndex) const;
   bool IsPerspective() const;
 
 private:
@@ -231,7 +230,6 @@ private:
   dp::UniformValuesStorage m_generalUniforms;
 
   bool m_enable3dInNavigation;
-  bool m_isBillboardRenderPass;
 
   Viewport m_viewport;
   UserEventStream m_userEventStream;
