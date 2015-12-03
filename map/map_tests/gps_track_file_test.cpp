137b026dcc51d73f34db77a82f6a7f13865fa3fe
Constantin Shalnev <c.shalnev@corp.mail.ru>
2015-12-03 10:55:50 +0300

    Added DropInMiddle test

--- map/map_tests/gps_track_file_test.cpp
+++ map/map_tests/gps_track_file_test.cpp
@@ -135,3 +135,35 @@ UNIT_TEST(GpsTrackFile_DropInTail)
 
   file.Close();
 }
+
+UNIT_TEST(GpsTrackFile_DropInMiddle)
+{
+  time_t const timestamp = system_clock::to_time_t(system_clock::now());
+  string const filePath = my::JoinFoldersToPath(GetPlatform().WritableDir(), "gpstrack.bin");
+
+  GpsTrackFile file(filePath, 100);
+
+  file.Clear();
+
+  for (size_t i = 0; i < 50; ++i)
+    file.Append(timestamp + i, m2::PointD(i+1000,i+2000), i+3000);
+
+  TEST_EQUAL(50, file.GetCount(), ());
+
+  file.DropEarlierThan(timestamp + 48.5); // drop all except last
+
+  TEST_EQUAL(1, file.GetCount(), ());
+
+  size_t i = 49; // new first
+  file.ForEach([&i,timestamp](double t, m2::PointD const & pt, double speed)->bool
+  {
+    TEST_EQUAL(timestamp + i, t, ());
+    TEST_EQUAL(pt.x, i + 1000, ());
+    TEST_EQUAL(pt.y, i + 2000, ());
+    TEST_EQUAL(speed, i + 3000, ());
+    ++i;
+    return true;
+  });
+
+  file.Close();
+}
