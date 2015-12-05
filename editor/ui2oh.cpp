7807ddc22300611c058520e1f538d69698b37c71
Sergey Magidovich <mgsergio@mapswithme.com>
2015-12-05 10:07:09 +0300

    Switch to classes due to complexity of a task.

--- editor/ui2oh.cpp
+++ editor/ui2oh.cpp
@@ -1,10 +1,12 @@
 #include "editor/ui2oh.hpp"
 
 #include "std/array.hpp"
+#include "std/string.hpp"
 
+#include "3party/opening_hours/opening_hours.hpp"
 
-namespace
-{
+// namespace
+// {
 // osmoh::TWeekdayRanges DaysSetToRange( const & days)
 // {
 
@@ -26,21 +28,17 @@ namespace
 //   }
 //   return {};
 // }
-} // namespace
+// } // namespace
 
 namespace editor
 {
-// osmoh::OpeningHours ConvertOpeningkHours(ui::TTimeTables const & oh)
-// {
-//   using namespace osmoh;
+osmoh::OpeningHours ConvertOpeningHours(ui::TimeTableSet const & tt)
+{
+  return string(); // TODO(mgsergio): // Just a dummy.
+}
 
-//   // TRuleSequences rule;
-//   // TWeekdayRanges weekdayRanges;
-//   return {};
-// }
-
-ui::TTimeTables ConvertOpeningkHours(osmoh::OpeningHours const & oh)
+ui::TimeTableSet ConvertOpeningHours(osmoh::OpeningHours const & oh)
 {
   return {};
 }
-} // namespace
+} // namespace editor
