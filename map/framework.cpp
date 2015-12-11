dabd1253b9e5500d5048790597592a90514e4677
Constantin Shalnev <c.shalnev@corp.mail.ru>
2015-12-11 18:58:48 +0300

    Clear points on Disconnect only if Drape exists

--- map/framework.cpp
+++ map/framework.cpp
@@ -1274,9 +1274,7 @@ void Framework::CreateDrapeEngine(ref_ptr<dp::OGLContextFactory> contextFactory,
     InsertRoute(m_routingSession.GetRoute());
 
   if (m_connectToGpsTrack)
-  {
     GpsTracker::Instance().Connect(bind(&Framework::OnUpdateGpsTrackPointsCallback, this, _1, _2));
-  }
 }
 
 ref_ptr<df::DrapeEngine> Framework::GetDrapeEngine()
@@ -1287,7 +1285,6 @@ ref_ptr<df::DrapeEngine> Framework::GetDrapeEngine()
 void Framework::DestroyDrapeEngine()
 {
   GpsTracker::Instance().Disconnect();
-
   m_drapeEngine.reset();
 }
 
@@ -1304,8 +1301,9 @@ void Framework::ConnectToGpsTracker()
 void Framework::DisconnectFromGpsTracker()
 {
   m_connectToGpsTrack = false;
-  m_drapeEngine->ClearGpsTrackPoints();
   GpsTracker::Instance().Disconnect();
+  if (m_drapeEngine)
+    m_drapeEngine->ClearGpsTrackPoints();
 }
 
 void Framework::OnUpdateGpsTrackPointsCallback(vector<pair<size_t, location::GpsTrackInfo>> && toAdd,
