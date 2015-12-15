1261bcdeb506bfd2020450f935fbdfcaea5f1435
Vladimir Byko-Ianko <v.bykoianko@corp.mail.ru>
2015-12-15 16:55:28 +0300

    Using string index in TIndex to identify nodes in mwm tree. Removing country_tree, active_map_layout and country_bridge.

--- storage/index.cpp
+++ storage/index.cpp
@@ -5,13 +5,18 @@
 
 namespace storage
 {
-  // GCC bug? Can't move initialization to hpp file (linker error).
-  const int TIndex::INVALID = -1;
-
+  const string TIndex::INVALID = string("");
   string DebugPrint(TIndex const & r)
   {
-    ostringstream out;
-    out << "storage::TIndex(" << r.m_group << ", " << r.m_country << ", " << r.m_region << ")";
-    return out.str();
+    return r.m_idx;
   }
+  // GCC bug? Can't move initialization to hpp file (linker error).
+//  const int TIndex::INVALID = -1;
+
+//  string DebugPrint(TIndex const & r)
+//  {
+//    ostringstream out;
+//    out << "storage::TIndex(" << r.m_group << ", " << r.m_country << ", " << r.m_region << ")";
+//    return out.str();
+//  }
 }
