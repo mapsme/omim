02dff4429ef3b94b8d8b6d34deeffb260aa82c1d
Vladimir Byko-Ianko <v.bykoianko@corp.mail.ru>
2015-12-18 08:24:25 +0300

    All commented code was removed.

--- storage/index.hpp
+++ storage/index.hpp
@@ -12,24 +12,6 @@ namespace storage
     TIndex() : m_idx(INVALID) {}
     bool operator==(TIndex const & other) const { return m_idx == other.m_idx; }
     bool IsValid() const { return m_idx != INVALID; }
-
-//    static int const INVALID;
-
-//    int m_group;
-//    int m_country;
-//    int m_region;
-
-//    TIndex(int group = INVALID, int country = INVALID, int region = INVALID)
-//      : m_group(group), m_country(country), m_region(region) {}
-
-//    bool IsValid() const { return (m_group != INVALID && m_country != INVALID); }
-
-//    bool operator==(TIndex const & other) const
-//    {
-//      return (m_group == other.m_group &&
-//              m_country == other.m_country &&
-//              m_region == other.m_region);
-//    }
      
     bool operator!=(TIndex const & other) const
     {
@@ -40,15 +22,6 @@ namespace storage
     {
       return m_idx.compare(other.m_idx) > 0;
     }
-
-//    bool operator<(TIndex const & other) const
-//    {
-//      if (m_group != other.m_group)
-//        return m_group < other.m_group;
-//      else if (m_country != other.m_country)
-//        return m_country < other.m_country;
-//      return m_region < other.m_region;
-//    }
   };
 
   string DebugPrint(TIndex const & r);
