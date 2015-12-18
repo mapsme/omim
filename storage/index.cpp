e74329bb30c0056fd2f836064efc3a1b81d1489f
Vladimir Byko-Ianko <v.bykoianko@corp.mail.ru>
2015-12-18 08:24:25 +0300

    All commented code was removed.

--- storage/index.cpp
+++ storage/index.cpp
@@ -10,13 +10,4 @@ namespace storage
   {
     return r.m_idx;
   }
-  // GCC bug? Can't move initialization to hpp file (linker error).
-//  const int TIndex::INVALID = -1;
-
-//  string DebugPrint(TIndex const & r)
-//  {
-//    ostringstream out;
-//    out << "storage::TIndex(" << r.m_group << ", " << r.m_country << ", " << r.m_region << ")";
-//    return out.str();
-//  }
 }
