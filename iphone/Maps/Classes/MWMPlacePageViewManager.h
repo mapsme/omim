2ae5a8b5830b45170e2a75646cd4fc4ce2455e5d
Илья Гречухин <i.grechuhin@gmail.com>
2015-12-28 17:33:10 +0300

    [ios] Refactored place page.

--- iphone/Maps/Classes/MWMPlacePageViewManager.h
+++ iphone/Maps/Classes/MWMPlacePageViewManager.h
@@ -16,6 +16,7 @@
 - (instancetype)initWithViewController:(UIViewController *)viewController
                               delegate:(id<MWMPlacePageViewManagerProtocol>)delegate;
 - (void)showPlacePageWithUserMark:(unique_ptr<UserMarkCopy>)userMark;
+- (void)reloadPlacePage;
 - (void)refreshPlacePage;
 - (void)refresh;
 - (void)dismissPlacePage;
@@ -24,7 +25,7 @@
 - (void)routeFrom;
 - (void)routeTo;
 - (void)share;
-- (void)editPlace;
+- (void)editPlaceTime;
 - (void)addBookmark;
 - (void)removeBookmark;
 - (void)apiBack;
