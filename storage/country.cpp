bb5a3d112454619e94b0c86dd62d8e2ec347e32b
Vladimir Byko-Ianko <v.bykoianko@corp.mail.ru>
2016-01-06 12:40:30 +0300

    Corrections after colleagues comments.

--- storage/country.cpp
+++ storage/country.cpp
@@ -203,15 +203,15 @@ void SaveImpl(T const & v, json_t * jParent)
     my::JsonHandle jCountry;
     jCountry.AttachNew(json_object());
 
-    string const strName = v.At(i).Value().Name();
+    string const strName = v.Child(i).Value().Name();
     CHECK(!strName.empty(), ("Empty country name?"));
     json_object_set_new(jCountry.get(), "n", json_string(strName.c_str()));
 
-    size_t countriesCount = v.At(i).Value().GetFilesCount();
+    size_t countriesCount = v.Child(i).Value().GetFilesCount();
     ASSERT_LESS_OR_EQUAL(countriesCount, 1, ());
     if (countriesCount > 0)
     {
-      CountryFile const & file = v.At(i).Value().GetFile();
+      CountryFile const & file = v.Child(i).Value().GetFile();
       string const & strFile = file.GetName();
       if (strFile != strName)
         json_object_set_new(jCountry.get(), "f", json_string(strFile.c_str()));
@@ -220,8 +220,8 @@ void SaveImpl(T const & v, json_t * jParent)
                           json_integer(file.GetRemoteSize(MapOptions::CarRouting)));
     }
 
-    if (v.At(i).ChildrenCount())
-      SaveImpl(v.At(i), jCountry.get());
+    if (v.Child(i).ChildrenCount())
+      SaveImpl(v.Child(i), jCountry.get());
 
     json_array_append(jArray.get(), jCountry.get());
   }
