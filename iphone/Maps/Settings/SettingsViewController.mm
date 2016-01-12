80475e59d77ef250c66bb384b865693ba7200fc9
VladiMihaylenko <vxmihaylenko@gmail.com>
2016-01-12 18:08:13 +0300

    [ios] Night mode bugfix. (images)

--- iphone/Maps/Settings/SettingsViewController.mm
+++ iphone/Maps/Settings/SettingsViewController.mm
@@ -321,7 +321,7 @@ Settings::Units unitsForIndex(NSInteger index)
   }
   case SectionRouting:
     // Change TTS language
-    if (indexPath.row == 3)
+    if (indexPath.row == 2)
     {
       [[Statistics instance] logEvent:kStatEventName(kStatSettings, kStatTTS)
                      withParameters:@{kStatAction : kStatChangeLanguage}];
