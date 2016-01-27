296d4d09cbb6178398dbe9596c2ea913e4a5f27a
Dmitry Yunitsky <yunik@mapswithme.com>
2016-01-28 01:26:48 +0300

    [android] Get title for showcase menu item from myTarget sdk.

--- android/src/com/mapswithme/maps/MwmActivity.java
+++ android/src/com/mapswithme/maps/MwmActivity.java
@@ -810,7 +810,8 @@ public class MwmActivity extends BaseMwmFragmentActivity
           mMainMenu.showShowcase(false);
           return;
         }
-
+        final NativeAppwallBanner menuBanner = nativeAppwallAd.getBanners().get(0);
+        mMainMenu.setShowcaseText(menuBanner.getTitle());
         mMainMenu.showShowcase(true);
       }
 
