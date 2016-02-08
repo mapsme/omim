22ac69e356c5b533fb13a9c5d0d16800af9cfa17
Ilya Grechuhin <i.grechuhin@gmail.com>
2016-02-08 22:20:01 +0300

    [ios] Fixed build.

--- iphone/Maps/ViewController.h
+++ iphone/Maps/ViewController.h
@@ -1,5 +1,7 @@
 @interface ViewController : UIViewController
 
+@property (nonatomic, readonly) BOOL hasNavigationBar;
+
 - (void)mwm_refreshUI;
 
 @property (nonatomic, readonly) BOOL hasNavigationBar;
