e8530edc2958a97599874a4aa479c2d96651f891
Alexander Marchuk <alexm@maps.me>
2016-06-08 17:06:19 +0300

    [android][locator] refactor: GPS locator refactored.

--- android/src/com/mapswithme/maps/widget/placepage/PlacePageView.java
+++ android/src/com/mapswithme/maps/widget/placepage/PlacePageView.java
@@ -887,7 +887,7 @@ public class PlacePageView extends RelativeLayout implements View.OnClickListene
 
   public void refreshAzimuth(double northAzimuth)
   {
-    if (getState() == State.HIDDEN ||
+    if (isHidden() ||
         mMapObject == null ||
         MapObject.isOfType(MapObject.MY_POSITION, mMapObject))
       return;
@@ -1201,6 +1201,11 @@ public class PlacePageView extends RelativeLayout implements View.OnClickListene
   {
     detachCountry();
     setState(State.HIDDEN);
+  }
+
+  public boolean isHidden()
+  {
+    return (getState() == State.HIDDEN);
   }
 
   @SuppressWarnings("SimplifiableIfStatement")
