860f83722a3fe48289ec58d69145f57926329011
Arsentiy Milchakov <a.milchakov@corp.mail.ru>
2016-07-07 21:59:52 +0300

    utf8 multilang string refactoring

--- coding/multilang_utf8_string.cpp
+++ coding/multilang_utf8_string.cpp
@@ -63,72 +63,21 @@ char const * StringUtf8Multilang::GetLangNameByCode(int8_t langCode)
   return g_languages[langCode].m_name;
 }
 
-size_t StringUtf8Multilang::GetNextIndex(size_t i) const
-{
-  ++i;
-  size_t const sz = m_s.size();
-
-  while (i < sz && (m_s[i] & 0xC0) != 0x80)
-  {
-    if ((m_s[i] & 0x80) == 0)
-      i += 1;
-    else if ((m_s[i] & 0xFE) == 0xFE)
-      i += 7;
-    else if ((m_s[i] & 0xFC) == 0xFC)
-      i += 6;
-    else if ((m_s[i] & 0xF8) == 0xF8)
-      i += 5;
-    else if ((m_s[i] & 0xF0) == 0xF0)
-      i += 4;
-    else if ((m_s[i] & 0xE0) == 0xE0)
-      i += 3;
-    else if ((m_s[i] & 0xC0) == 0xC0)
-      i += 2;
-  }
-
-  return i;
-}
-
 void StringUtf8Multilang::AddString(int8_t lang, string const & utf8s)
 {
-  size_t i = 0;
-  size_t const sz = m_s.size();
+  ASSERT(lang < kMaxSupportedLanguages, ("Incorrect language index"));
 
-  while (i < sz)
-  {
-    size_t const next = GetNextIndex(i);
-
-    if ((m_s[i] & 0x3F) == lang)
-    {
-      ++i;
-      m_s.replace(i, next - i, utf8s);
-      return;
-    }
-
-    i = next;
-  }
-
-  m_s.push_back(lang | 0x80);
-  m_s.insert(m_s.end(), utf8s.begin(), utf8s.end());
+  m_strings[lang] = utf8s;
 }
 
 bool StringUtf8Multilang::GetString(int8_t lang, string & utf8s) const
 {
-  size_t i = 0;
-  size_t const sz = m_s.size();
+  auto it = m_strings.find(lang);
 
-  while (i < sz)
+  if (m_strings.end() != it)
   {
-    size_t const next = GetNextIndex(i);
-
-    if ((m_s[i] & 0x3F) == lang)
-    {
-      ++i;
-      utf8s.assign(m_s.c_str() + i, next - i);
-      return true;
-    }
-
-    i = next;
+    utf8s = it->second;
+    return true;
   }
 
   return false;
@@ -136,7 +85,7 @@ bool StringUtf8Multilang::GetString(int8_t lang, string & utf8s) const
 
 namespace
 {
-
+  
 struct Printer
 {
   string & m_out;
--- coding/multilang_utf8_string.hpp
+++ coding/multilang_utf8_string.hpp
@@ -4,6 +4,8 @@
 
 #include "base/assert.hpp"
 
+#include "std/algorithm.hpp"
+#include "std/map.hpp"
 #include "std/string.hpp"
 
 
@@ -30,9 +32,7 @@ template <class TSource> void ReadString(TSource & src, string & s)
 
 class StringUtf8Multilang
 {
-  string m_s;
-
-  size_t GetNextIndex(size_t i) const;
+  map<int8_t, string> m_strings;
 
 public:
   static int8_t constexpr kUnsupportedLanguageCode = -1;
@@ -63,17 +63,18 @@ public:
 
   inline bool operator== (StringUtf8Multilang const & rhs) const
   {
-    return (m_s == rhs.m_s);
+    if (m_strings.size() != rhs.m_strings.size())
+      return false;
+
+    return equal(m_strings.begin(), m_strings.end(), rhs.m_strings.begin());
   }
 
   inline bool operator!= (StringUtf8Multilang const & rhs) const
   {
     return !(*this == rhs);
   }
-
-  inline void Clear() { m_s.clear(); }
-  inline bool IsEmpty() const { return m_s.empty(); }
-
+  inline void Clear() { m_strings.clear(); }
+  inline bool IsEmpty() const { return m_strings.empty(); }
   void AddString(int8_t lang, string const & utf8s);
   void AddString(string const & lang, string const & utf8s)
   {
@@ -85,14 +86,10 @@ public:
   template <class T>
   void ForEach(T && fn) const
   {
-    size_t i = 0;
-    size_t const sz = m_s.size();
-    while (i < sz)
+    for (auto const & item : m_strings)
     {
-      size_t const next = GetNextIndex(i);
-      if (!fn((m_s[i] & 0x3F), m_s.substr(i + 1, next - i - 1)))
+      if (!fn(item.first, item.second))
         return;
-      i = next;
     }
   }
 
@@ -110,12 +107,57 @@ public:
 
   template <class TSink> void Write(TSink & sink) const
   {
-    utils::WriteString(sink, m_s);
+    string str;
+    for (auto const & item : m_strings)
+    {
+      str.push_back(item.first | 0x80);
+      str.insert(str.end(), item.second.begin(), item.second.end());
+    }
+
+    utils::WriteString(sink, str);
   }
 
   template <class TSource> void Read(TSource & src)
   {
-    utils::ReadString(src, m_s);
+    m_strings.clear();
+
+    string str;
+    utils::ReadString(src, str);
+
+    size_t i = 0;
+    size_t const sz = str.size();
+
+    auto getNextIndex = [&str](size_t i) -> size_t {
+      ++i;
+      size_t const sz = str.size();
+
+      while (i < sz && (str[i] & 0xC0) != 0x80)
+      {
+        if ((str[i] & 0x80) == 0)
+          i += 1;
+        else if ((str[i] & 0xFE) == 0xFE)
+          i += 7;
+        else if ((str[i] & 0xFC) == 0xFC)
+          i += 6;
+        else if ((str[i] & 0xF8) == 0xF8)
+          i += 5;
+        else if ((str[i] & 0xF0) == 0xF0)
+          i += 4;
+        else if ((str[i] & 0xE0) == 0xE0)
+          i += 3;
+        else if ((str[i] & 0xC0) == 0xC0)
+          i += 2;
+      }
+
+      return i;
+    };
+
+    while (i < sz)
+    {
+      size_t const next = getNextIndex(i);
+      m_strings.emplace((str[i] & 0x3F), str.substr(i + 1, next - i - 1));
+      i = next;
+    }
   }
 };
 
