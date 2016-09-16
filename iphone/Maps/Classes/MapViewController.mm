815920049f13e4f78a8d161e910fbc24d00727c7
Ilya Grechuhin <i.grechuhin@gmail.com>
2016-09-16 10:13:27 +0300

    [ios] Added profile & booking what’s new.

--- iphone/Maps/Classes/MapViewController.mm
+++ iphone/Maps/Classes/MapViewController.mm
@@ -26,7 +26,7 @@
 #import "MWMSettings.h"
 #import "MWMStorage.h"
 #import "MWMTableViewController.h"
-#import "MWMWhatsNewNavigationController.h"
+#import "MWMWhatsNewProfileBookingController.h"
 #import "MapsAppDelegate.h"
 #import "Statistics.h"
 #import "UIColor+MapsMeColor.h"
@@ -305,7 +305,7 @@ BOOL gIsFirstMyPositionMode = YES;
   if (isIOS7)
     return;
 
-  Class<MWMWelcomeControllerProtocol> whatsNewClass = [MWMWhatsNewNavigationController class];
+  Class<MWMWelcomeControllerProtocol> whatsNewClass = [MWMWhatsNewProfileBookingController class];
   BOOL const isFirstSession = [Alohalytics isFirstSession];
   Class<MWMWelcomeControllerProtocol> welcomeClass =
       isFirstSession ? [MWMFirstLaunchController class] : whatsNewClass;
