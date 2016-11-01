310c666b163c2e8200e9dc38f30f7ef09083e8f5
Roman Romanov <rromanov@65gb.net>
2016-11-01 10:22:08 +0400

    [android] Fixed PP my position altitude display.

--- android/src/com/mapswithme/maps/MwmActivity.java
+++ android/src/com/mapswithme/maps/MwmActivity.java
@@ -1024,11 +1024,6 @@ public class MwmActivity extends BaseMwmFragmentActivity
       request.setPointData(object.getLat(), object.getLon(), object.getTitle(), object.getApiId());
       object.setSubtitle(request.getCallerName(MwmApplication.get()).toString());
     }
-    else if (MapObject.isOfType(MapObject.MY_POSITION, object) &&
-             RoutingController.get().isNavigating())
-    {
-      return;
-    }
 
     setFullscreen(false);
 
