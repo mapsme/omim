72fe269fcc4e608feb776ea079fe84b7a416ed70
hunterisx <hunterisx@gmail.com>
2016-11-15 01:54:53 +0200

    Generated translations
    Generated translations

--- tools/unix/generate_localizations.sh
+++ tools/unix/generate_localizations.sh
@@ -10,5 +10,5 @@ $TWINE --format android generate-all-string-files "$OMIM_PATH/strings.txt" "$OMI
 $TWINE --format apple generate-all-string-files "$OMIM_PATH/strings.txt" "$OMIM_PATH/iphone/Maps/"
 $TWINE --format apple --file-name InfoPlist.strings generate-all-string-files "$OMIM_PATH/iphone/plist.txt" "$OMIM_PATH/iphone/Maps/"
 $TWINE --format jquery generate-all-string-files "$OMIM_PATH/data/cuisines.txt" "$OMIM_PATH/data/cuisine-strings/"
-$TWINE --format jquery generate-all-string-files "$OMIM_PATH/data/countries_names.txt" "$OMIM_PATH/data/countries-strings/"
+$TWINE --format jquery generate-all-string-files "$OMIM_PATH/data/lithuania.txt" "$OMIM_PATH/data/countries-strings/"
 #$TWINE --format tizen generate-all-string-files "$OMIM_PATH/strings.txt" "$OMIM_PATH/tizen/MapsWithMe/res/" --tags tizen
