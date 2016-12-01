04f95b19fb2a34d82a9b462fcea179e81d53655f
Vladimir Byko-Ianko <v.bykoianko@corp.mail.ru>
2016-12-01 15:44:22 +0300

    Preventing combinatorial explosion of graph nodes while adding restrictions.

--- routing/index_graph.hpp
+++ routing/index_graph.hpp
@@ -333,6 +333,8 @@ private:
   bool ApplyRestrictionPrepareData(RestrictionPoint const & restrictionPoint,
                                    RestrictionInfo & restrictionInfo);
 
+  void InsertToEdgeMapping(DirectedEdge const & key, DirectedEdge const & value);
+
   Geometry m_geometry;
   shared_ptr<EdgeEstimator> m_estimator;
   RoadIndex m_roadIndex;
