98f2316997e9046cd914c849c88034a63588e49f
VladiMihaylenko <vxmihaylenko@gmail.com>
2017-02-01 12:42:13 +0300

    [ios] Attempt to fix crash with place page opening hours layout.

--- iphone/Maps/UI/PlacePage/PlacePageLayout/Content/OpeningHoursCell/MWMOpeningHoursLayoutHelper.mm
+++ iphone/Maps/UI/PlacePage/PlacePageLayout/Content/OpeningHoursCell/MWMOpeningHoursLayoutHelper.mm
@@ -160,9 +160,15 @@ NSAttributedString * richStringFromDay(osmoh::Day const & day, BOOL isClosedNow)
 
         if (self.isExtended)
         {
-          [self extendMetainfoRowsWithSize:ip.count];
-          [tableView insertRowsAtIndexPaths:ip
-                           withRowAnimation:UITableViewRowAnimationLeft];
+          if ([self extendMetainfoRowsWithSize:ip.count])
+          {
+            [tableView insertRowsAtIndexPaths:ip
+                             withRowAnimation:UITableViewRowAnimationLeft];
+          }
+          else
+          {
+            LOG(LWARNING, ("Incorrect raw opening hours string: ", self.rawString.UTF8String));
+          }
         }
         else
         {
@@ -197,12 +203,12 @@ NSAttributedString * richStringFromDay(osmoh::Day const & day, BOOL isClosedNow)
   }
 }
 
-- (void)extendMetainfoRowsWithSize:(NSUInteger)size
+- (BOOL)extendMetainfoRowsWithSize:(NSUInteger)size
 {
   if (size == 0)
   {
-    NSAssert(false, @"Incorrect number of days!");
-    return;
+    LOG(LWARNING, ("Incorrect number of days!"));
+    return NO;
   }
 
   auto & metainfoRows = self.data.mutableMetainfoRows;
@@ -212,10 +218,11 @@ NSAttributedString * richStringFromDay(osmoh::Day const & day, BOOL isClosedNow)
   if (it == metainfoRows.end())
   {
     LOG(LERROR, ("Incorrect state!"));
-    return;
+    return NO;
   }
 
   metainfoRows.insert(++it, size, MetainfoRows::ExtendedOpeningHours);
+  return YES;
 }
 
 - (void)reduceMetainfoRows
