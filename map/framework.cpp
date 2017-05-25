6a8847da2095f754273422121ebfa134f6962e93
Ilya Grechuhin <i.grechuhin@gmail.com>
2017-05-25 16:04:28 +0300

    [FABRIC-7708] Fixed special search marks for local ads and tinkoff objects.

--- map/framework.cpp
+++ map/framework.cpp
@@ -1739,7 +1739,8 @@ void Framework::FillSearchResultsMarks(search::Results const & results)
     {
       SearchMarkPoint * mark = static_cast<SearchMarkPoint *>(guard.m_controller.CreateUserMark(r.GetFeatureCenter()));
       ASSERT_EQUAL(mark->GetMarkType(), UserMark::Type::SEARCH, ());
-      if (r.GetResultType() == search::Result::RESULT_FEATURE)
+      auto const isFeature = r.GetResultType() == search::Result::RESULT_FEATURE;
+      if (isFeature)
         mark->SetFoundFeature(r.GetFeatureID());
       mark->SetMatchedName(r.GetString());
 
@@ -1747,7 +1748,7 @@ void Framework::FillSearchResultsMarks(search::Results const & results)
         mark->SetCustomSymbol("search-booking");
       else if (r.m_metadata.m_isSponsoredBank)
         mark->SetCustomSymbol("search-tinkoff");
-      else if (m_localAdsManager.Contains(r.GetFeatureID()))
+      else if (isFeature && m_localAdsManager.Contains(r.GetFeatureID()))
         mark->SetCustomSymbol("search-adv");
     }
   }
