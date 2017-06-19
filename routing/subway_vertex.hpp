db5fe2eaccf98ffb7053177ed9194bce56aed8c4
Добрый Ээх <bukharaev@gmail.com>
2017-06-19 18:23:19 +0300

    [routing] Subway type enum

--- routing/subway_vertex.hpp
+++ routing/subway_vertex.hpp
@@ -8,13 +8,19 @@
 
 namespace routing
 {
+enum class SubwayType : uint8_t
+{
+  Line,
+  Change,
+};
+
 class SubwayVertex final
 {
 public:
   SubwayVertex() = default;
 
-  SubwayVertex(NumMwmId mwmId, uint32_t featureId, uint32_t pointId)
-    : m_mwmId(mwmId), m_featureId(featureId), m_pointId(pointId)
+  SubwayVertex(NumMwmId mwmId, uint32_t featureId, uint32_t pointId, SubwayType type)
+    : m_mwmId(mwmId), m_featureId(featureId), m_pointId(pointId), m_type(type)
   {
   }
 
@@ -24,9 +30,12 @@ public:
 
   uint32_t GetPointId() const { return m_pointId; }
 
+  SubwayType GetType() const { return m_type; }
+
   bool operator==(SubwayVertex const & rp) const
   {
-    return m_mwmId == rp.m_mwmId && m_featureId == rp.m_featureId && m_pointId == rp.m_pointId;
+    return m_mwmId == rp.m_mwmId && m_featureId == rp.m_featureId && m_pointId == rp.m_pointId &&
+           m_type == rp.m_type;
   }
 
   bool operator!=(SubwayVertex const & rp) const
@@ -42,19 +51,33 @@ public:
     if (m_featureId != rp.m_featureId)
       return m_featureId < rp.m_featureId;
 
-    return m_pointId < rp.m_pointId;
+    if (m_pointId != rp.m_pointId)
+      return m_pointId < rp.m_pointId;
+
+    return m_type < rp.m_type;
   }
 
 private:
   NumMwmId m_mwmId = kFakeNumMwmId;
   uint32_t m_featureId = 0;
   uint32_t m_pointId = 0;
+  SubwayType m_type = SubwayType::Line;
 };
+
+inline std::string DebugPrint(SubwayType type)
+{
+  switch (type)
+  {
+  case SubwayType::Line: return "line";
+  case SubwayType::Change: return "change";
+  }
+}
 
 inline std::string DebugPrint(SubwayVertex const & rp)
 {
   std::ostringstream out;
-  out << "SubwayVertex [" << rp.GetMwmId() << rp.GetFeatureId() << ", " << rp.GetPointId() << "]";
+  out << "SubwayVertex [" << rp.GetMwmId() << rp.GetFeatureId() << ", " << rp.GetPointId() << ", "
+      << DebugPrint(rp.GetType()) << "]";
   return out.str();
 }
 }  // namespace routing
