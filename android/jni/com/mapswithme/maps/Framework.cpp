ce986d4d5492ccbce0fe5fea1c91a1cc5b4f6c7b
Александр Зацепин <az@mapswithme.com>
2017-06-21 09:50:20 +0300

    [android] Added getUgcApi method to Framework

--- android/jni/com/mapswithme/maps/Framework.cpp
+++ android/jni/com/mapswithme/maps/Framework.cpp
@@ -560,6 +560,15 @@ uber::RideRequestLinks Framework::GetUberLinks(string const & productId, ms::Lat
   return uber::Api::GetRideRequestLinks(productId, from, to);
 }
 
+void Framework::RequestUGC(JNIEnv * env, ugc::Api::UGCCallback const & ugcCallback)
+{
+  ugc::Api & ugcApi = m_work.GetUGCApi();
+
+  auto const & info = g_framework->GetPlacePageInfo();
+
+  ugcApi.GetUGC(info.GetID(), ugcCallback);
+}
+
 int Framework::ToDoAfterUpdate() const
 {
   return (int) m_work.ToDoAfterUpdate();
