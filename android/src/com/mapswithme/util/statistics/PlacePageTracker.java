1fdbcb54b5785406c02547bcf31e1430d402cfb2
Александр Зацепин <az@mapswithme.com>
2017-07-12 15:02:30 +0300

    [android] Added viator view to PlacePageTracker

--- android/src/com/mapswithme/util/statistics/PlacePageTracker.java
+++ android/src/com/mapswithme/util/statistics/PlacePageTracker.java
@@ -10,10 +10,9 @@ import com.mapswithme.maps.R;
 import com.mapswithme.maps.bookmarks.data.MapObject;
 import com.mapswithme.maps.taxi.TaxiManager;
 import com.mapswithme.maps.widget.placepage.PlacePageView;
+import com.mapswithme.util.UiUtils;
 
 import java.util.List;
-
-import static android.view.View.INVISIBLE;
 
 public class PlacePageTracker
 {
@@ -22,15 +21,19 @@ public class PlacePageTracker
   private final PlacePageView mPlacePageView;
   @NonNull
   private final View mTaxi;
+  @NonNull
+  private final View mViator;
   @Nullable
   private MapObject mMapObject;
 
-  private boolean mTracked;
+  private boolean mTaxiTracked;
+  private boolean mViatorTracked;
 
   public PlacePageTracker(@NonNull PlacePageView placePageView)
   {
     mPlacePageView = placePageView;
     mTaxi = mPlacePageView.findViewById(R.id.ll__place_page_taxi);
+    mViator = mPlacePageView.findViewById(R.id.ll__place_viator);
     Activity activity = (Activity) placePageView.getContext();
     mBottomPadding = activity.getResources().getDimensionPixelOffset(R.dimen.place_page_buttons_height);
   }
@@ -43,16 +46,18 @@ public class PlacePageTracker
   public void onMove()
   {
     trackTaxiVisibility();
+    trackViatorVisibility();
   }
 
   public void onHide()
   {
-    mTracked = false;
+    mTaxiTracked = false;
+    mViatorTracked = false;
   }
 
   private void trackTaxiVisibility()
   {
-    if (!mTracked && isViewOnScreen(mTaxi) && mMapObject != null)
+    if (!mTaxiTracked && isViewOnScreen(mTaxi) && mMapObject != null)
     {
       List<Integer> taxiTypes = mMapObject.getReachableByTaxiTypes();
       if (taxiTypes != null && !taxiTypes.isEmpty())
@@ -60,21 +65,43 @@ public class PlacePageTracker
         @TaxiManager.TaxiType
         int type = taxiTypes.get(0);
         Statistics.INSTANCE.trackTaxiEvent(Statistics.EventName.ROUTING_TAXI_REAL_SHOW_IN_PP, type);
-        mTracked = true;
+        mTaxiTracked = true;
       }
+    }
+  }
+
+  private void trackViatorVisibility()
+  {
+    if (!mViatorTracked && isViewOnScreen(mViator) && mMapObject != null)
+    {
+      mViatorTracked = true;
     }
   }
 
   private boolean isViewOnScreen(@NonNull View view) {
 
-    if (mPlacePageView.getVisibility() == INVISIBLE)
+    if (UiUtils.isInvisible(mPlacePageView) || UiUtils.isInvisible(view) || !view.isShown())
       return false;
 
     Rect localRect = new Rect();
     Rect globalRect = new Rect();
     view.getLocalVisibleRect(localRect);
     view.getGlobalVisibleRect(globalRect);
-    return localRect.bottom >= view.getHeight() && localRect.top == 0
+
+    return UiUtils.isLandscape(view.getContext()) ? isPartiallyVisible(view, localRect, globalRect)
+                              : isCompletelyVisible(view, localRect, globalRect);
+  }
+
+  private boolean isCompletelyVisible(@NonNull View view, @NonNull Rect localRect,
+                                      @NonNull Rect globalRect)
+  {
+    return view.getHeight() > 0 && localRect.bottom >= view.getHeight() && localRect.top == 0
            && globalRect.bottom <= mPlacePageView.getBottom() - mBottomPadding;
+  }
+
+  private boolean isPartiallyVisible(@NonNull View view, @NonNull Rect localRect,
+                                     @NonNull Rect globalRect)
+  {
+    return view.getHeight() > 0 && localRect.top == 0 || globalRect.bottom < mPlacePageView.getBottom();
   }
 }
