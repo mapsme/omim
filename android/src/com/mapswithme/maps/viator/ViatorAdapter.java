5aba3702a92da1e133602f022c0d30bd6271c2ea
Александр Зацепин <az@mapswithme.com>
2017-08-09 11:54:18 +0300

    [andorid] Addded localized title/subtitle for loading view for viator

--- android/src/com/mapswithme/maps/viator/ViatorAdapter.java
+++ android/src/com/mapswithme/maps/viator/ViatorAdapter.java
@@ -12,6 +12,7 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.mapswithme.maps.MwmApplication;
 import com.mapswithme.maps.R;
 import com.mapswithme.maps.base.BaseSponsoredAdapter;
 import com.mapswithme.maps.widget.placepage.Sponsored;
@@ -22,6 +23,10 @@ import java.util.List;
 
 public final class ViatorAdapter extends BaseSponsoredAdapter
 {
+  private static final String LOADING_TITLE = MwmApplication
+      .get().getString(R.string.preloader_viator_title);
+  private static final String LOADING_SUBTITLE = MwmApplication
+      .get().getString(R.string.preloader_viator_message);
   public ViatorAdapter(@NonNull String url, boolean hasError,
                        @Nullable ItemSelectedListener listener)
   {
@@ -69,16 +74,14 @@ public final class ViatorAdapter extends BaseSponsoredAdapter
   @Override
   protected String getLoadingTitle()
   {
-    //TODO return localized resource
-    return "title";
+    return LOADING_TITLE;
   }
 
   @Nullable
   @Override
   protected String getLoadingSubtitle()
   {
-    //TODO return localized resource
-    return "subtitle";
+    return LOADING_SUBTITLE;
   }
 
   @LayoutRes
