b245a7043650ffbcb9f4c633950eae7a7ba20f9c
tatiana-kondakova <tatiana.kondakova@gmail.com>
2017-10-17 15:53:24 +0300

    Add TransitInfo to RouteSegment to draw transit route properly

--- routing/transit_graph.hpp
+++ routing/transit_graph.hpp
@@ -39,6 +39,10 @@ public:
             std::vector<transit::Edge> const & edges, EdgeEstimator const & estimator,
             NumMwmId numMwmId, IndexGraph & indexGraph);
 
+  bool IsGate(Segment const & segment) const;
+  bool IsEdge(Segment const & segment) const;
+  transit::Edge const & GetEdge(Segment const & segment) const;
+
 private:
   Segment GetTransitSegment(uint32_t segmentIdx) const;
   Segment GetNewTransitSegment() const;
@@ -49,9 +53,6 @@ private:
   void AddConnections(map<transit::StopId, std::set<transit::Edge>> const & connections,
                       bool isOutgoing);
 
-  bool IsGate(Segment const & segment) const;
-  bool IsEdge(Segment const & segment) const;
-  transit::Edge const & GetEdge(Segment const & segment) const;
   transit::Gate const & GetGate(Segment const & segment) const;
 
   static uint32_t constexpr kTransitFeatureId = FakeFeatureIds::kTransitGraphId;
