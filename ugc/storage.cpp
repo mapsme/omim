9a9d22e046d769162f4147799ae360cc7eb4a888
VladiMihaylenko <vxmihaylenko@gmail.com>
2017-10-23 14:30:37 +0300

    Using optionals in ftraits

--- ugc/storage.cpp
+++ ugc/storage.cpp
@@ -122,8 +122,8 @@ void Storage::SetUGCUpdate(FeatureID const & id, UGCUpdate const & ugc)
   auto const mercator = feature::GetCenter(*feature);
   feature::TypesHolder th(*feature);
   th.SortBySpec();
-  uint32_t matchingType = 0;
-  CHECK(ftraits::UGC::GetType(th, matchingType), ());
+  auto const optMatchingType = ftraits::UGC::GetType(th);
+  CHECK(optMatchingType, ());
   auto const type = th.GetBestType();
   for (auto & index : m_UGCIndexes)
   {
@@ -143,7 +143,7 @@ void Storage::SetUGCUpdate(FeatureID const & id, UGCUpdate const & ugc)
 
   index.m_mercator = mercator;
   index.m_type = type;
-  index.m_matchingType = matchingType;
+  index.m_matchingType = *optMatchingType;
   index.m_mwmName = id.GetMwmName();
   index.m_dataVersion = id.GetMwmVersion();
   index.m_featureId = id.m_index;
