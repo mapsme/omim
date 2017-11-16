f5b881e9ddc2c1cacf565b3ac728a7b0677c4d8d
Arsentiy Milchakov <milcars@mapswithme.com>
2017-11-16 15:22:06 +0300

    [booking][jni] jni layer for booking online filter

---
+++ android/src/com/mapswithme/maps/search/BookingFilterParams.java
@@ -0,0 +1,46 @@
+package com.mapswithme.maps.search;
+
+import android.support.annotation.NonNull;
+
+class BookingFilterParams
+{
+  static class Room
+  {
+    private int mAdultsCount;
+    private int mAgeOfChild;
+
+    Room(int adultsCount, int ageOfChild)
+    {
+      mAdultsCount = adultsCount;
+      mAgeOfChild = ageOfChild;
+    }
+  }
+
+  private long mCheckinMillisec;
+  private long  mCheckoutMillisec;
+  @NonNull
+  private Room[] mRooms;
+
+  BookingFilterParams(long checkinMillisec, long checkoutMillisec, @NonNull Room[] rooms)
+  {
+    mCheckinMillisec = checkinMillisec;
+    mCheckoutMillisec = checkoutMillisec;
+    mRooms = rooms;
+  }
+
+  public long getCheckinMillisec()
+  {
+    return mCheckinMillisec;
+  }
+
+  public long getCheckoutMillisec()
+  {
+    return mCheckoutMillisec;
+  }
+
+  @NonNull
+  public Room[] getRooms()
+  {
+    return mRooms;
+  }
+}
