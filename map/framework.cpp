9ee07d0a57857ae41c10c2295eb4473651b662fe
Arsentiy Milchakov <milcars@mapswithme.com>
2017-12-12 11:05:17 +0300

    [cian] extended preview is disabled

--- map/framework.cpp
+++ map/framework.cpp
@@ -887,7 +887,6 @@ void Framework::FillInfoFromFeatureType(FeatureType const & ft, place_page::Info
     info.SetSponsoredType(place_page::SponsoredType::Viator);
     auto const & sponsoredId = info.GetMetadata().Get(feature::Metadata::FMD_SPONSORED_ID);
     info.SetSponsoredUrl(viator::Api::GetCityUrl(sponsoredId));
-    info.SetPreviewIsExtended();
   }
   else if (ftypes::IsHotelChecker::Instance()(ft))
   {
@@ -901,7 +900,6 @@ void Framework::FillInfoFromFeatureType(FeatureType const & ft, place_page::Info
   {
     info.SetSponsoredType(SponsoredType::Cian);
     info.SetSponsoredUrl(cian::Api::GetMainPageUrl());
-    info.SetPreviewIsExtended();
   }
   else if (ftypes::IsThorChecker::Instance()(ft) &&
            !info.GetMetadata().Get(feature::Metadata::FMD_RATING).empty())
