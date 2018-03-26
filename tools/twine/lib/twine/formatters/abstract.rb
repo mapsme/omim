c656125835c827bd0a599e552b09936811c2a8d8
greshilov <slovaricheg@gmail.com>
2018-03-26 20:59:48 +0300

    [twine] Add plural strings support

--- tools/twine/lib/twine/formatters/abstract.rb
+++ tools/twine/lib/twine/formatters/abstract.rb
@@ -46,11 +46,11 @@ module Twine
           end
           current_definition = TwineDefinition.new(key)
           current_section.definitions << current_definition
-          
+
           if @options[:tags] && @options[:tags].length > 0
-            current_definition.tags = @options[:tags]            
+            current_definition.tags = @options[:tags]
           end
-          
+
           @twine_file.definitions_by_key[key] = current_definition
           @twine_file.definitions_by_key[key].translations[lang] = value
         else
@@ -63,12 +63,12 @@ module Twine
 
       def set_comment_for_key(key, comment)
         return unless @options[:consume_comments]
-        
+
         if @twine_file.definitions_by_key.include?(key)
           definition = @twine_file.definitions_by_key[key]
-          
-          reference = @twine_file.definitions_by_key[definition.reference_key] if definition.reference_key
 
+          reference = @twine_file.definitions_by_key[definition.reference_key] if definition.reference_key
+          
           if !reference or comment != reference.raw_comment
             definition.comment = comment
           end
