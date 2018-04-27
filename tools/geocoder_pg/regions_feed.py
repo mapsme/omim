9b5996da55314bf501f2f6886eaf0cbc314804ad
Ilya Zverev <zverik@textual.ru>
2018-04-27 15:58:09 +0300

    Simplify the regions_feed script

--- tools/geocoder_pg/regions_feed.py
+++ tools/geocoder_pg/regions_feed.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import psycopg2
 import json
-import argparse
 
 
 DB_NODE_BASE = 50*1000*1000*1000
@@ -14,43 +13,37 @@ class OsmIdCode:
     RESET = ~(NODE | WAY | RELATION)
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser('Prepares a json feed with regions for geocoder')
-    parser.add_argument('-d', '--database', default='borders',
-                        help='Database name, default=borders')
-    parser.add_argument('-t', '--table', default='osm_polygon',
-                        help='Database table, default=osm_polygon')
-    parser.add_argument('output', type=argparse.FileType('w'),
-                        help='Output file, use "-" for stdout')
-    options = parser.parse_args()
+def osmid_to_rid(osm_id):
+    if osm_id < 0:
+        rid = (-osm_id) | OsmIdCode.RELATION
+    elif osm_id > DB_NODE_BASE:
+        rid = (osm_id - DB_NODE_BASE) | OsmIdCode.NODE
+    else:
+        rid = osm_id | OsmIdCode.WAY
+    if rid >= 2**63:
+        # Negate as in int64_t
+        rid = -1 - (rid ^ (2**64 - 1))
+    return rid
 
-    cursor = psycopg2.connect('dbname='+options.database).cursor()
+
+if __name__ == '__main__':
+    cursor = psycopg2.connect('dbname=borders').cursor()
     cursor.execute('''select osm_id, level, name, rank, ST_X(centroid), ST_Y(centroid)
-                   from {0} where rank <= 14 and name is not null order by rank'''.format(
-                       options.table))
+                   from osm_polygon where rank <= 14 and name is not null order by rank''')
 
     for region in cursor.fetchall():
         osm_id, level, name, rank, lon, lat = region
-        cursor.execute('''select level, name
-                       from {0} p join {0}_geom g using (osm_id)
+        cursor.execute('''select level, osm_id, name
+                       from osm_polygon p join osm_polygon_geom g using (osm_id)
                        where ST_Intersects(g.way, ST_SetSRID(ST_MakePoint(%s, %s), 4326))
-                       and rank < %s
-                       '''.format(options.table), (lon, lat, rank))
+                       and rank < %s''', (lon, lat, rank))
         addr = {level: name}
         for row in cursor:
-            addr[row[0]] = row[1]
-        if osm_id < 0:
-            rid = (-osm_id) | OsmIdCode.RELATION
-        elif osm_id > DB_NODE_BASE:
-            rid = (osm_id - DB_NODE_BASE) | OsmIdCode.NODE
-        else:
-            rid = osm_id | OsmIdCode.WAY
-        if rid >= 2**63:
-            # Negate as in int64_t
-            rid = -1 - (rid ^ (2**64 - 1))
+            addr[row[0]] = row[2]
+            addr[row[0]+'_id'] = osmid_to_rid(row[1])
         feature = {
             'type': 'Feature',
             'geometry': {'type': 'Point', 'coordinates': [lon, lat]},
             'properties': {'name': name, 'rank': rank, 'address': addr}
         }
-        options.output.write(str(rid) + ' ' + json.dumps(feature, ensure_ascii=False) + '\n')
+        print(str(osmid_to_rid(osm_id)) + ' ' + json.dumps(feature, ensure_ascii=False))
