589bacdc53a2b05a0984301e2a36ed9c42d06ba5
r.kuznetsov <r.kuznetsov@corp.mail.ru>
2018-07-30 19:16:42 +0300

    [Core] Added user agent for http requests

--- map/user.cpp
+++ map/user.cpp
@@ -481,6 +481,7 @@ void User::RequestImpl(std::string const & url, BuildRequestHandler const & onBu
 
   platform::HttpClient request(url);
   request.SetRawHeader("Accept", kApplicationJson);
+  request.SetUserAgent(GetPlatform().GetAppUserAgent());
   if (onBuildRequest)
     onBuildRequest(request);
 
