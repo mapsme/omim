21a58d015808195ee2b57e0da9d066e44fcb36e8
r.kuznetsov <r.kuznetsov@corp.mail.ru>
2018-07-31 12:41:09 +0300

    Review fixes

--- android/src/com/mapswithme/maps/location/AndroidNativeProvider.java
+++ android/src/com/mapswithme/maps/location/AndroidNativeProvider.java
@@ -8,7 +8,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.mapswithme.maps.MwmApplication;
-import com.mapswithme.util.LocationUtils;
 
 import java.util.ArrayList;
 import java.util.List;
