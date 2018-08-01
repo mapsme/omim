c7bf07458367517bcd635e6b01c7c8c3e50887cf
r.kuznetsov <r.kuznetsov@corp.mail.ru>
2018-08-01 15:29:18 +0300

    Removed FC2018 from code

--- map/framework.cpp
+++ map/framework.cpp
@@ -1481,7 +1481,7 @@ search::DisplayedCategories const & Framework::GetDisplayedCategories()
     city = m_cityFinder->GetCityName(*position, StringUtf8Multilang::kEnglishCode);
 
   // Apply sponsored modifiers.
-  std::tuple<LuggageHeroModifier, Fc2018Modifier> modifiers(city, city);
+  std::tuple<LuggageHeroModifier> modifiers(city);
   my::for_each_in_tuple(modifiers, [&](size_t, SponsoredCategoryModifier & modifier)
   {
     m_displayedCategories->Modify(modifier);
@@ -1625,10 +1625,6 @@ void Framework::FillSearchResultsMarks(search::Results::ConstIter begin,
       mark->SetFoundFeature(r.GetFeatureID());
 
     mark->SetMatchedName(r.GetString());
-
-    // TODO: Remove after FC2018 finishing.
-    if (r.m_metadata.m_isFootballCupObject)
-      mark->SetMarkType(SearchMarkType::Fc2018);
 
     if (r.m_metadata.m_isSponsoredHotel)
     {
