3aad4e0cf464a5b1ff71f27e0aace6a185f9d110
Alexander Zatsepin <az@mapswithme.com>
2018-08-02 21:08:38 +0300

    [android] Implemented 'download guides' button logic

--- android/src/com/mapswithme/maps/bookmarks/BookmarksCatalogActivity.java
+++ android/src/com/mapswithme/maps/bookmarks/BookmarksCatalogActivity.java
@@ -1,5 +1,7 @@
 package com.mapswithme.maps.bookmarks;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -10,12 +12,33 @@ import com.mapswithme.maps.R;
 import com.mapswithme.maps.SplashActivity;
 import com.mapswithme.maps.base.BaseToolbarActivity;
 import com.mapswithme.maps.base.OnBackPressListener;
+import com.mapswithme.maps.bookmarks.data.BookmarkManager;
 import com.mapswithme.util.Utils;
 
 public class BookmarksCatalogActivity extends BaseToolbarActivity
 {
   public static final int REQ_CODE_CATALOG = 101;
   public static final String EXTRA_DOWNLOADED_CATEGORY = "extra_downloaded_category";
+
+  public static void startForResult(@NonNull Fragment fragment, int requestCode)
+  {
+    fragment.startActivityForResult(makeLaunchIntent(fragment.getContext()), requestCode);
+  }
+
+  public static void startForResult(@NonNull Activity context, int requestCode)
+  {
+    context.startActivityForResult(makeLaunchIntent(context), requestCode);
+  }
+
+  @NonNull
+  private static Intent makeLaunchIntent(@NonNull Context context)
+  {
+    Intent intent = new Intent(context, BookmarksCatalogActivity.class);
+    intent.putExtra(BookmarksCatalogFragment.EXTRA_BOOKMARKS_CATALOG_URL,
+                    BookmarkManager.INSTANCE.getCatalogFrontendUrl());
+    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    return intent;
+  }
 
   @Override
   protected void safeOnCreate(@Nullable Bundle savedInstanceState)
