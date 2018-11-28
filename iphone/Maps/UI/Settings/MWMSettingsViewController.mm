2453abcc3f6a806faf35291139c7d790d7edd496
Aleksey Belouosv <aleksey@maps.me>
2018-11-28 15:28:19 +0300

    [iOS] remove ads in settings

--- iphone/Maps/UI/Settings/MWMSettingsViewController.mm
+++ iphone/Maps/UI/Settings/MWMSettingsViewController.mm
@@ -12,7 +12,7 @@
 
 extern NSString * const kAlohalyticsTapEventKey;
 
-@interface MWMSettingsViewController ()<SettingsTableViewSwitchCellDelegate>
+@interface MWMSettingsViewController ()<SettingsTableViewSwitchCellDelegate, RemoveAdsViewControllerDelegate>
 
 @property(weak, nonatomic) IBOutlet SettingsTableViewLinkCell * profileCell;
 
@@ -141,9 +141,12 @@ extern NSString * const kAlohalyticsTapEventKey;
                                             title:L(@"pref_calibration_title")
                                              isOn:[MWMSettings compassCalibrationEnabled]];
 
+  auto & purchase = GetFramework().GetPurchase();
+  bool const hasSubscription = purchase && purchase->IsSubscriptionActive(SubscriptionType::RemoveAds);
   [self.showOffersCell configWithDelegate:self
                                     title:L(@"showcase_settings_title")
-                                     isOn:![MWMSettings adForbidden]];
+                                     isOn:!hasSubscription];
+  self.showOffersCell.isEnabled = !hasSubscription;
 
   [self.statisticsCell configWithDelegate:self
                                     title:L(@"allow_statistics")
@@ -197,6 +200,13 @@ extern NSString * const kAlohalyticsTapEventKey;
 {
   [self.helpCell configWithTitle:L(@"help") info:nil];
   [self.aboutCell configWithTitle:L(@"about_menu_title") info:nil];
+}
+
+- (void)showRemoveAds
+{
+  auto removeAds = [[RemoveAdsViewController alloc] init];
+  removeAds.delegate = self;
+  [self.navigationController presentViewController:removeAds animated:YES completion:nil];
 }
 
 #pragma mark - SettingsTableViewSwitchCellDelegate
@@ -254,9 +264,9 @@ extern NSString * const kAlohalyticsTapEventKey;
   }
   else if (cell == self.showOffersCell)
   {
+    [self showRemoveAds];
     [Statistics logEvent:kStatEventName(kStatSettings, kStatAd)
           withParameters:@{kStatAction : kStatAd, kStatValue : (value ? kStatOn : kStatOff)}];
-    [MWMSettings setAdForbidden:!value];
   }
   else if (cell == self.statisticsCell)
   {
@@ -402,6 +412,21 @@ extern NSString * const kAlohalyticsTapEventKey;
   case 1: return L(@"allow_statistics_hint");
   default: return nil;
   }
+}
+
+#pragma mark - RemoveAdsViewControllerDelegate
+
+- (void)didCompleteSubscribtion:(RemoveAdsViewController *)viewController
+{
+  [self.navigationController dismissViewControllerAnimated:YES completion:nil];
+  self.showOffersCell.isEnabled = NO;
+}
+
+- (void)didCancelSubscribtion:(RemoveAdsViewController *)viewController
+{
+  [self.navigationController dismissViewControllerAnimated:YES completion:^{
+    self.showOffersCell.isOn = YES;
+  }];
 }
 
 @end
