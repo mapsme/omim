c2a89e305f49a2185f67c1fb84b5a989cecd8707
tatiana-yan <tatiana.kondakova@gmail.com>
2018-12-11 13:20:47 +0300

    [RELEASE_ONLY][search] Revert LocalitiesSource fix for 8.4 data compatibility.

--- search/localities_source.cpp
+++ search/localities_source.cpp
@@ -8,10 +8,14 @@ LocalitiesSource::LocalitiesSource()
 {
   auto & c = classif();
 
-  auto const city = c.GetTypeByPath({"place", "city"});
-  c.ForEachInSubtree([this](uint32_t c) { m_cities.push_back(c); }, city);
+//  auto const city = c.GetTypeByPath({"place", "city"});
+//  c.ForEachInSubtree([this](uint32_t c) { m_cities.push_back(c); }, city);
 
-  auto const town = c.GetTypeByPath({"place", "town"});
-  c.ForEachInSubtree([this](uint32_t t) { m_towns.push_back(t); }, town);
+//  auto const town = c.GetTypeByPath({"place", "town"});
+//  c.ForEachInSubtree([this](uint32_t t) { m_towns.push_back(t); }, town);
+
+  // RELEASE_ONLY: revert LocalitiesSource fix for 8.4 data compatibility.
+  m_cities.push_back(c.GetTypeByPath({"place", "city"}));
+  m_towns.push_back(c.GetTypeByPath({"place", "town"}));
 }
 }  // namespace search
