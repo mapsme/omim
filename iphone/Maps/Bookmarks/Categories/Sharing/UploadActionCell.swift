9b8614251f8f29f1dccd8506b6abb14f528ed11e
Aleksey Belouosv <aleksey@maps.me>
2019-03-01 16:07:05 +0300

    [iOS] change text in direct link cell if list in public
    https://jira.mail.ru/browse/MAPSME-9871

--- iphone/Maps/Bookmarks/Categories/Sharing/UploadActionCell.swift
+++ iphone/Maps/Bookmarks/Categories/Sharing/UploadActionCell.swift
@@ -70,6 +70,7 @@ final class UploadActionCell: MWMTableViewCell {
         actionImage.tintColor = .blackSecondaryText()
         actionTitle.textColor = .blackSecondaryText()
         actionTitle.font = .regular16()
+        actionTitle.text = titles?[.disabled]
         shareButton.isHidden = true
         selectionStyle = .none
       }
