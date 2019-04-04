a428a350a65cdbb0453bc25c8839602e235a1887
Mikhail Gorbushin <m.gorbushin@corp.mail.ru>
2019-04-04 14:01:17 +0300

    [routing] revert file: routing/index_graph_starter.cpp

--- routing/index_graph_starter.cpp
+++ routing/index_graph_starter.cpp
@@ -169,7 +169,7 @@ void IndexGraphStarter::GetEdgesList(Segment const & segment, bool isOutgoing,
   edges.clear();
 
   // If mode is LeapsOnly we need to connect start/finish segment to transitions.
-  if (m_graph.GetMode() == WorldGraphMode::LeapsOnly)
+  if (m_graph.GetMode() == WorldGraph::Mode::LeapsOnly)
   {
     if (segment.IsRealSegment() && !IsRoutingOptionsGood(segment))
       return;
@@ -363,12 +363,12 @@ void IndexGraphStarter::AddFakeEdges(Segment const & segment, bool isOutgoing, v
       //     |segment|       |s|
       //  *------------>*----------->
       bool const sIsOutgoing =
-          GetJunction(segment, true /* front */) == GetJunction(s, false /* front */);
+          GetJunction(segment, true /*front */) == GetJunction(s, false /* front */);
 
       //        |s|       |segment|
       //  *------------>*----------->
       bool const sIsIngoing =
-          GetJunction(s, true /* front */) == GetJunction(segment, false /* front */);
+          GetJunction(s, true /*front */) == GetJunction(segment, false /* front */);
 
       if ((isOutgoing && sIsOutgoing) || (!isOutgoing && sIsIngoing))
       {
