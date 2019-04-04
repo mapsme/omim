200bf048ad0f2adc12defd897ab0ccda6fa9bf6d
Mikhail Gorbushin <m.gorbushin@corp.mail.ru>
2019-04-04 16:35:04 +0300

    [routing] del debug

--- routing/index_router.cpp
+++ routing/index_router.cpp
@@ -543,10 +543,6 @@ RouterResultCode IndexRouter::CalculateSubroute(Checkpoints const & checkpoints,
       break;
   }
 
-  bool no_leaps = std::getenv("NO_LEAPS") && !std::string(std::getenv("NO_LEAPS")).empty();
-  if (no_leaps)
-    starter.GetGraph().SetMode(WorldGraphMode::Joints);
-
   LOG(LINFO, ("Routing in mode:", starter.GetGraph().GetMode()));
 
   auto const progressRange = CalcProgressRange(checkpoints, subrouteIdx);
