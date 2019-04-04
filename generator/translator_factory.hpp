2bca2fa711e1acc3506d4363aad5597c144ce30b
Mikhail Gorbushin <m.gorbushin@corp.mail.ru>
2019-04-04 16:24:23 +0300

    [routing] revert file: generator/translator_factory.hpp

--- generator/translator_factory.hpp
+++ generator/translator_factory.hpp
@@ -32,7 +32,6 @@ std::shared_ptr<TranslatorInterface> CreateTranslator(TranslatorType type, Args&
   case TranslatorType::GeoObjects:
     return create<TranslatorGeoObjects>(std::forward<Args>(args)...);
   }
-
   UNREACHABLE();
 }
 }  // namespace generator
