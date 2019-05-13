1478a451ecd38e26c7d0c0fbb83ed896de73ba7f
r.kuznetsov <r.kuznetsov@corp.mail.ru>
2019-05-13 15:06:35 +0300

    [drape] Fixed map selection

--- drape_frontend/frontend_renderer.cpp
+++ drape_frontend/frontend_renderer.cpp
@@ -2310,6 +2310,8 @@ void FrontendRenderer::OnContextCreate()
 
 void FrontendRenderer::OnRenderingEnabled()
 {
+  m_overlayTree->InvalidateOnNextFrame();
+
 #ifndef DRAPE_MEASURER_BENCHMARK
   DrapeMeasurer::Instance().Start();
 #endif
