ea23637548489e7011b8d1c156b89fcf0117d2ce
Maksim Andrianov <maksimandrianov1@gmail.com>
2019-06-24 03:11:28 +0300

    +

--- generator/generator_tool/generator_tool.cpp
+++ generator/generator_tool/generator_tool.cpp
@@ -306,7 +306,7 @@ int GeneratorToolMain(int argc, char ** argv)
       !FLAGS_generate_streets_features.empty() || 
       !FLAGS_generate_geo_objects_features.empty())
   {
-    RawGenerator rawGenerator(genInfo, 1);
+    RawGenerator rawGenerator(genInfo, threadsCount);
     if (FLAGS_generate_features)
       rawGenerator.GenerateCountries(FLAGS_no_ads);
     if (FLAGS_generate_world)
