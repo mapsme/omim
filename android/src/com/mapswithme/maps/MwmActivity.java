1f1ad5f5d1be56cf6f82b0a3bf4474f0014089b7
Dmitry Donskoy <donskdmitry@mail.ru>
2019-08-23 16:09:05 +0300

    [android] Added a refresh of the "filter and show on the map" view when user moved to the search screen first time

--- android/src/com/mapswithme/maps/MwmActivity.java
+++ android/src/com/mapswithme/maps/MwmActivity.java
@@ -510,6 +510,7 @@ public class MwmActivity extends BaseMwmFragmentActivity
 
     mSearchController = new FloatingSearchToolbarController(this, this);
     mSearchController.setVisibilityListener(this);
+    SearchEngine.INSTANCE.addListener(this);
 
     SharingHelper.INSTANCE.initialize();
 
@@ -1287,7 +1288,6 @@ public class MwmActivity extends BaseMwmFragmentActivity
   protected void onStart()
   {
     super.onStart();
-    SearchEngine.INSTANCE.addListener(this);
     Framework.nativeSetMapObjectListener(this);
     BookmarkManager.INSTANCE.addLoadingListener(this);
     BookmarkManager.INSTANCE.addCatalogListener(this);
@@ -1302,7 +1302,6 @@ public class MwmActivity extends BaseMwmFragmentActivity
   protected void onStop()
   {
     super.onStop();
-    SearchEngine.INSTANCE.removeListener(this);
     Framework.nativeRemoveMapObjectListener();
     BookmarkManager.INSTANCE.removeLoadingListener(this);
     BookmarkManager.INSTANCE.removeCatalogListener(this);
@@ -1328,6 +1327,7 @@ public class MwmActivity extends BaseMwmFragmentActivity
     mToggleMapLayerController.detachCore();
     TrafficManager.INSTANCE.detachAll();
     mPlacePageController.destroy();
+    SearchEngine.INSTANCE.removeListener(this);
   }
 
   @Override
