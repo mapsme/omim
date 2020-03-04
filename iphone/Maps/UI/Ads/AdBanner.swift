5ea722d6f0ea7936cdea7346350e94f47bf65a13
Alexander Boriskov <a.boriskov@corp.mail.ru>
2020-03-04 20:57:59 +0300

    [iOS] Changed NativeAd to NativeBannerAd in Facebook SDK

--- iphone/Maps/UI/Ads/AdBanner.swift
+++ iphone/Maps/UI/Ads/AdBanner.swift
@@ -176,7 +176,7 @@ final class AdBanner: UITableViewCell {
       adCallToActionButtonDetailed.isHighlighted = highlighted;
   }
 
-  private func configFBBanner(ad: FBNativeAd) {
+  private func configFBBanner(ad: FBNativeBannerAd) {
     adType = .native
     DAAImageWidth.constant = adPrivacyImage.width;
     DAAImage.isHidden = false;
@@ -188,7 +188,6 @@ final class AdBanner: UITableViewCell {
       adCallToActionButtons = [adCallToActionButtonCompact, adCallToActionButtonDetailed]
     }
     ad.registerView(forInteraction: self,
-                    mediaView: FBMediaView(),
                     iconImageView: adIconImageView,
                     viewController: UIViewController.topViewController(),
                     clickableViews: adCallToActionButtons)
@@ -235,8 +234,7 @@ final class AdBanner: UITableViewCell {
       adCallToActionButtons = [adCallToActionButtonCompact, adCallToActionButtonDetailed]
       adCallToActionButtons.forEach { $0.setTitle(ad.ctaText, for: .normal) }
     }
-    mpNativeAd?.setAdView(self, actionButtons: adCallToActionButtons)
-
+    mpNativeAd?.setAdView(self, iconView: adIconImageView, actionButtons: adCallToActionButtons)
     adTitleLabel.attributedText = attributedTitle(title: ad.title,
                                                   indent: adPrivacyImage.width + DAAImageWidth.constant)
     adBodyLabel.text = ad.text
