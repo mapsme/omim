86e8bc7e6538a74ea8e6d2dcc7a43ff0a4a1fed4
Aleksey Belousov <aleksey@maps.me>
2020-04-27 16:01:12 +0300

    [iOS] set correct button state when appears in 'more' popup in PP
    https://jira.mail.ru/browse/MAPSME-13770

--- iphone/Maps/UI/PlacePage/PlacePageLayout/Layouts/PlacePageCommonLayout.swift
+++ iphone/Maps/UI/PlacePage/PlacePageLayout/Layouts/PlacePageCommonLayout.swift
@@ -354,6 +354,7 @@ extension PlacePageCommonLayout {
     } else {
       hidden = true
     }
+    self.presenter?.layoutIfNeeded()
     UIView.animate(withDuration: kDefaultAnimationDuration) { [unowned self] in
       self.bookmarkViewController.view.isHidden = hidden
       self.presenter?.layoutIfNeeded()
