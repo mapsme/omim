1f8f985284f766d1dcc2285df3d4e0dc95d8091a
Aleksey Belousov <aleksey@maps.me>
2020-08-18 17:33:28 +0300

    [iOS] fix state preservation in search hotels params
    Also adjust guests picker available values
    
    https://jira.mail.ru/browse/MAPSME-14446
    https://jira.mail.ru/browse/MAPSME-14453

--- iphone/Maps/UI/Search/MWMSearchManager.mm
+++ iphone/Maps/UI/Search/MWMSearchManager.mm
@@ -83,6 +83,8 @@ using Observers = NSHashTable<Observer>;
   if (self.state != MWMSearchManagerStateHidden)
     self.state = MWMSearchManagerStateDefault;
   self.searchTextField.text = @"";
+  [self.searchBarView resetDates];
+  [self.searchBarView resetGuestCount];
   [MWMSearch clear];
 }
 
@@ -139,6 +141,11 @@ using Observers = NSHashTable<Observer>;
 
   DatePickerViewController *controller = [[DatePickerViewController alloc] init];
   controller.delegate = self;
+  MWMHotelParams *filter = [MWMSearch getFilter];
+  if (filter != nil && filter.checkInDate != nil && filter.checkOutDate != nil) {
+    controller.initialCheckInDate = filter.checkInDate;
+    controller.initialCheckOutDate = filter.checkOutDate;
+  }
   controller.popoverPresentationController.sourceView = self.searchBarView;
   controller.popoverPresentationController.permittedArrowDirections = UIPopoverArrowDirectionUp;
   [[MapViewController sharedController] presentViewController:controller animated:YES completion:nil];
@@ -157,6 +164,13 @@ using Observers = NSHashTable<Observer>;
 
   GuestsPickerViewController *controller = [[GuestsPickerViewController alloc] init];
   controller.delegate = self;
+  MWMHotelParams *filter = [MWMSearch getFilter];
+  if (filter != nil) {
+    controller.roomsInitialCount = filter.numberOfRooms;
+    controller.adultsInitialCount = filter.numberOfAdults;
+    controller.childrenInitialCount = filter.numberOfChildren;
+    controller.infantsInitialCount = filter.numberOfInfants;
+  }
   controller.popoverPresentationController.sourceView = self.searchBarView;
   controller.popoverPresentationController.permittedArrowDirections = UIPopoverArrowDirectionUp;
   [[MapViewController sharedController] presentViewController:controller animated:YES completion:nil];
