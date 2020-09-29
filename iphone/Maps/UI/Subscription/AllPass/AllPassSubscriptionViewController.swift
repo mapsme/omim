11020856fee2aab26d8c504b06175163d889cc6c
Alexander Boriskov <a.boriskov@corp.mail.ru>
2020-09-29 16:32:48 +0300

    [iOS] Fixed crash when open AllPass Subscription from richmedia
    https://jira.mail.ru/browse/MAPSME-14934

--- iphone/Maps/UI/Subscription/AllPass/AllPassSubscriptionViewController.swift
+++ iphone/Maps/UI/Subscription/AllPass/AllPassSubscriptionViewController.swift
@@ -57,17 +57,26 @@ class AllPassSubscriptionViewController: UIViewController {
     backgroundImageView.images = [
       UIImage(named: "AllPassSubscriptionBg1"),
       UIImage(named: "AllPassSubscriptionBg2"),
-      UIImage(named: "AllPassSubscriptionBg3")
+      UIImage(named: "AllPassSubscriptionBg3"),
     ]
     startAnimating()
 
     let fontSize: CGFloat = UIScreen.main.bounds.width > 320 ? 17.0 : 14.0
+    let fontColor = UIColor.whitePrimaryText()
     let fontFamily = UIFont.systemFont(ofSize: fontSize).familyName
     let css = "<style type=\"text/css\">b{font-weight: 900;}body{font-weight: 300; font-size: \(fontSize); font-family: '-apple-system','\(fontFamily)';}</style>"
-    zip(descriptionSubtitles, ["all_pass_subscription_message_subtitle",
-                               "all_pass_subscription_message_subtitle_3",
-                               "all_pass_subscription_message_subtitle_2"]).forEach { title, loc in
-                                title.attributedText = NSAttributedString.string(withHtml: css + L(loc), defaultAttributes: [:])
+    
+    DispatchQueue.global().async {
+      let titles = ["all_pass_subscription_message_subtitle",
+                    "all_pass_subscription_message_subtitle_3",
+                    "all_pass_subscription_message_subtitle_2"].map { (key) -> NSAttributedString? in
+                      NSAttributedString.string(withHtml: css + L(key), defaultAttributes: [.foregroundColor: fontColor])
+                    }
+      DispatchQueue.main.async {
+        zip(self.descriptionSubtitles, titles).forEach { title, str in
+          title.attributedText = str
+        }
+      }
     }
 
     preferredContentSize = CGSize(width: 414, height: contentView.frame.height)
