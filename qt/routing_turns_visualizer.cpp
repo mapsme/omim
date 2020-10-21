9f981c8961730f35047436f1781bf2089205b3a0
Olga Khlopkova <o.khlopkova@corp.mail.ru>
2020-10-21 18:19:03 +0300

    [release-only] Revert "fix"
    This reverts commit f19c1a4c83cc2da9456ff2daa3035c36ff208aa0.

--- qt/routing_turns_visualizer.cpp
+++ qt/routing_turns_visualizer.cpp
@@ -30,7 +30,7 @@ void RoutingTurnsVisualizer::ClearTurns(df::DrapeApi & drape)
   m_turnIds.clear();
 }
 
-std::string RoutingTurnsVisualizer::GetId(routing::turns::TurnItem const & turn)
+static std::string RoutingTurnsVisualizer::GetId(routing::turns::TurnItem const & turn)
 {
   std::string const maneuver = turn.m_pedestrianTurn == routing::turns::PedestrianDirection::None
                                    ? DebugPrint(turn.m_turn)
