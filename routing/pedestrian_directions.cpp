67a445c74ee152faaf377f817de95e653e67062c
Olga Khlopkova <o.khlopkova@corp.mail.ru>
2020-10-21 18:15:49 +0300

    [release-only] Revert "[routing] Use of custom RoutingSettings by vehicle type in TurnsGenerator."
    This reverts commit 32403553de0c306101d32a1203db41891fc8bf6d.

--- routing/pedestrian_directions.cpp
+++ routing/pedestrian_directions.cpp
@@ -35,9 +35,7 @@ bool PedestrianDirectionsEngine::Generate(IndexRoadGraph const & graph,
 
   vector<Edge> routeEdges;
 
-  CHECK_NOT_EQUAL(m_vehicleType, VehicleType::Count, (m_vehicleType));
-
-  if (m_vehicleType == VehicleType::Transit)
+  if (m_isTransit)
   {
     routeGeometry = path;
     graph.GetRouteSegments(segments);
@@ -67,9 +65,8 @@ bool PedestrianDirectionsEngine::Generate(IndexRoadGraph const & graph,
     return false;
 
   RoutingEngineResult resultGraph(routeEdges, m_adjacentEdges, m_pathSegments);
-  auto const res =
-      MakeTurnAnnotationPedestrian(resultGraph, *m_numMwmIds, m_vehicleType, cancellable,
-                                   routeGeometry, turns, streetNames, segments);
+  auto const res = MakeTurnAnnotationPedestrian(resultGraph, *m_numMwmIds, cancellable,
+                                                routeGeometry, turns, streetNames, segments);
 
   if (res != RouterResultCode::NoError)
     return false;
