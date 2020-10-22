511ba694cb9916745760033a8ee500ca39b4cd88
Alexey <osminin.alexey@gmail.com>
2020-10-22 15:43:06 +0300

    Revert "[android] hotel rating string for PP"
    This reverts commit 21ea6b6f9854e12af380c0b49245eac8b28b4026.

--- android/src/com/mapswithme/maps/widget/placepage/PlacePageView.java
+++ android/src/com/mapswithme/maps/widget/placepage/PlacePageView.java
@@ -922,8 +922,7 @@ public class PlacePageView extends NestedScrollViewClickFixed
       UiUtils.show(mHotelReview);
       mReviewAdapter.setItems(new ArrayList<>(Arrays.asList(info.mReviews)));
       //noinspection ConstantConditions
-      mHotelRating.setText(getContext().getString(R.string.place_page_booking_rating,
-                                                  mSponsored.getRating()));
+      mHotelRating.setText(mSponsored.getRating());
       int reviewsCount = (int) info.mReviewsAmount;
       String text = getResources().getQuantityString(
           R.plurals.placepage_summary_rating_description, reviewsCount, reviewsCount);
