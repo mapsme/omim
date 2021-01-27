b5b0dbf6444256f863b690cf91d2bcf0ba3011da
Arsentiy Milchakov <milcars@mapswithme.com>
2021-01-27 18:54:28 +0300

    [downloader] the method IsUrlSupported is added

--- platform/downloader_utils.hpp
+++ platform/downloader_utils.hpp
@@ -7,5 +7,6 @@ namespace downloader
 {
 std::string GetFileDownloadUrl(std::string const & fileName, int64_t dataVersion,
                                uint64_t diffVersion);
+bool IsUrlSupported(std::string const & url);
 std::string GetFilePathByUrl(std::string const & url);
 }  // namespace downloader
