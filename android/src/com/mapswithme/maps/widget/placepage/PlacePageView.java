7dd0e8ad51538e2853d21c8691d87543c191c486
Arsentiy Milchakov <milcars@mapswithme.com>
2021-02-02 18:39:16 +0300

    [android] downloading progress tracking is added into DownloaderFragment, UpdaterDialogFragment and PlacePageView

--- android/src/com/mapswithme/maps/widget/placepage/PlacePageView.java
+++ android/src/com/mapswithme/maps/widget/placepage/PlacePageView.java
@@ -55,6 +55,7 @@ import com.mapswithme.maps.bookmarks.data.Metadata;
 import com.mapswithme.maps.bookmarks.data.RoadWarningMarkType;
 import com.mapswithme.maps.downloader.CountryItem;
 import com.mapswithme.maps.downloader.DownloaderStatusIcon;
+import com.mapswithme.maps.downloader.MapDownloadManager;
 import com.mapswithme.maps.downloader.MapManager;
 import com.mapswithme.maps.editor.Editor;
 import com.mapswithme.maps.editor.OpeningHours;
@@ -2106,6 +2107,7 @@ public class PlacePageView extends NestedScrollViewClickFixed
     mCurrentCountry = map;
     if (mStorageCallbackSlot == 0)
       mStorageCallbackSlot = MapManager.nativeSubscribe(mStorageCallback);
+    MapDownloadManager.from(getContext()).startProgressTracking();
 
     mDownloaderIcon.setOnIconClickListener(mDownloadClickListener)
                    .setOnCancelClickListener(mCancelDownloadListener);
@@ -2121,6 +2123,7 @@ public class PlacePageView extends NestedScrollViewClickFixed
 
     MapManager.nativeUnsubscribe(mStorageCallbackSlot);
     mStorageCallbackSlot = 0;
+    MapDownloadManager.from(getContext()).stopProgressTracking();
     mCurrentCountry = null;
     mDownloaderIcon.setOnIconClickListener(null)
                    .setOnCancelClickListener(null);
